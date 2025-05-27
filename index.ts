32aa551e997bdf9bbab6c614f48e95950ccf216d
shariqazeem <shariqshaukat786@gmail.com>
2025-05-27 11:39:41 +0500

    enhanced profile completion and some bugs fix

--- index.ts
+++ index.ts
@@ -1,6 +1,20 @@
 import WebServer from '@blockless/sdk-ts/dist/lib/web';
+import { readFileSync } from "fs";
+import { join } from "path";
+
 const server = new WebServer();
 
+// Serve static files (optional)
 server.statics('public', '/');
-server.start();
 
+
+
+
+server.get("/app.js", async (_, res) => {
+    const scriptPath = join(__dirname, "script.js");
+    const script = readFileSync(scriptPath, "utf-8");
+    return res.send(script, 200, { "Content-Type": "application/javascript" });
+});
+
+// Start the server
+server.start();
